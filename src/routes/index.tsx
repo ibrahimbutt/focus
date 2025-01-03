f2e27ae2ffc59479ae4ce6fbf8bc58f5a6b17e7f
Ibrahim Butt <ibrahimbutt1993@gmail.com>
2025-01-03 00:11:16 +0000

    chore: add start session button

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,8 +7,11 @@ export const Route = createFileRoute("/")({
 function IndexComponent() {
   return (
     <div className="flex items-center h-svh">
-      <div className="flex items-center justify-center h-full w-96 bg-neutral-50">
+      <div className="flex flex-col items-center justify-center h-full gap-3 w-96 bg-neutral-50">
         <div className="text-6xl font-medium">25:00</div>
+        <button className="px-3 text-white bg-blue-600 rounded-lg h-9">
+          Start Session
+        </button>
       </div>
       <div className="flex-1"></div>
     </div>
